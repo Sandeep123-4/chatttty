f0b87a4f89a0dc4b3cf8c1fe773e4eebf0e471b8
Sandeep123-4 <xtry122@gmail.com>
2026-01-30 11:50:15 -0500

    Update server.cpp

--- chattt/src/server.cpp
+++ chattt/src/server.cpp
@@ -1,33 +1,30 @@
-// server.cpp
 #include <iostream>
-#include <thread>
 #include <vector>
-#include <string>
+#include <thread>
 #include <mutex>
-#include <unistd.h>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 std::vector<int> clients;
 std::mutex clients_mutex;
 
-void broadcast(const std::string& message, int sender_fd) {
+void broadcast(const std::string &message, int sender_fd) {
     std::lock_guard<std::mutex> lock(clients_mutex);
     for (int client_fd : clients) {
-        if (client_fd != sender_fd) {
-            send(client_fd, message.c_str(), message.size(), 0);
-        }
+        if (client_fd != sender_fd) send(client_fd, message.c_str(), message.size(), 0);
     }
 }
 
 void handle_client(int client_fd) {
     char buffer[1024];
     while (true) {
-        int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-        if (bytes_received <= 0) break;
-        buffer[bytes_received] = '\0';
-        std::string msg = buffer;
-        std::cout << "Message: " << msg << std::endl;
-        broadcast(msg, client_fd);
+        int bytes = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes <= 0) break;
+        buffer[bytes] = '\0';
+        broadcast(buffer, client_fd);
     }
     close(client_fd);
     std::lock_guard<std::mutex> lock(clients_mutex);
@@ -35,28 +32,18 @@ void handle_client(int client_fd) {
 }
 
 int main() {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        std::cerr << "Socket creation failed\n";
-        return 1;
-    }
+    int port = 12345;
+    if (getenv("PORT")) port = std::stoi(getenv("PORT"));
 
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in server_addr{};
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(12345); // port
+    server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Bind failed\n";
-        return 1;
-    }
-
-    if (listen(server_fd, 5) < 0) {
-        std::cerr << "Listen failed\n";
-        return 1;
-    }
-
-    std::cout << "Server listening on port 12345...\n";
+    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    listen(server_fd, 5);
+    std::cout << "Server listening on port " << port << std::endl;
 
     while (true) {
         int client_fd = accept(server_fd, nullptr, nullptr);
@@ -70,3 +57,5 @@ int main() {
     close(server_fd);
     return 0;
 }
+
+
